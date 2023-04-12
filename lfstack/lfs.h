718f5f9fdb59fd6a7febba27aae7df873281209a
rcallan353 <rivers5533@DESKTOP-2KUF9AN>
2023-04-11 20:33:27 -0400

    added a note for the lock free stackk

--- lfstack/lfs.h
+++ lfstack/lfs.h
@@ -5,6 +5,17 @@
 #include <atomic>
 #include <iostream>
 
+/*
+note: the CountedNodePtr is a pretty simple struct, so we should be able to use it with the std::atomic<> template for the head of the list.
+on platforms that support a double word compare and swap operation, this structure will be small enough for std::atomic<CountedNodePtr>
+to be lock free. if this operation isn't available on your platform, it might be better to look for a different strategy for making your stack
+lock free
+
+possible alternativve solution: if your system has spare bits in the pointer, eg the address space is only 48 bits but a pointer is 64 bits,
+you could store the counter in the spare bits to fit the struct in a single machine word
+*/
+
+
 template <typename T>
 class LockFreeStack {
 private:
